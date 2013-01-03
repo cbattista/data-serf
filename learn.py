780e1acb61ce6e2dad3d7b0d384f7c4988c1fdd4
cbattista <battista.christian@gmail.com>
2013-01-02 20:01:07 -0500

    learn page

---
+++ learn.py
@@ -0,0 +1,28 @@
+import os
+import mt
+
+import cherrypy
+from mako_defs import *
+from config import *
+import lg_authority
+
+class learn(object):
+
+
+	@cherrypy.expose
+	def index(self):
+		output = ""
+
+		output += getLearnContent()
+
+		output = getPage(output, 'Datamaster How-To')
+
+		return output 
+
+
+if __name__ == '__main__':
+
+	cherrypy.config.update({'server.socket_port':learn_port})
+	cherrypy.config.update(cherry_settings)
+	cherrypy.quickstart(learn())
+
