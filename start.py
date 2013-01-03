ed5695207021e53a756b3d61e935d06775111fd3
cbattista <battista.christian@gmail.com>
2013-01-03 13:16:53 -0500

    start script

---
+++ start.py
@@ -0,0 +1,10 @@
+import os
+
+os.system('python index.py &')
+os.system('python learn.py &')
+os.system('python upload.py &')
+os.system('python manage.py &')
+os.system('python modify.py &')
+os.system('python download.py &')
+
+
