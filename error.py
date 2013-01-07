55f3f2d338a6b7d29b581583534b2870a0444380
Christian Battista <battista.christian@gmail.com>
2013-01-07 00:29:29 -0500

    added text for multiple error pages

--- error.py
+++ error.py
@@ -19,18 +19,25 @@ from mako_defs import *
 from config import *
 import cherrypy
 
-f = open('50x.html', 'w')
+d = {}
+d['50x'] = """<h3>attempting to perform a labour</h3><h2>that he could not perform</h2>"""
+ 
+d['403'] = """<h3>attempting to walk a path</h3><h2>which was forbidden to him</h2>"""
+d['404'] = """<h3>attempting to find a place</h3><h2>which did not exist</h2>"""
 
-img = """
+for page in ['50x', '403', '404']:
+
+	f = open('%s.html' % page, 'w')
+
+	img = """
 		<img src='%s/fallen_serf.png' align='right'/>
 		<h1>oh my</h1>
 		<h3>he has fallen</h3>
-		<h3>attempting to perform a labour</h3>
-		<h2>whose secrets were unknown to him</h2>
-	  """ % domain
+		%s
+	""" % (domain, d[page])
 
-pageContent = getPage(img, '', static=True)
+	pageContent = getPage(img, '', static=True)
 
-f.write(pageContent)
+	f.write(pageContent)
 
 f.close()
